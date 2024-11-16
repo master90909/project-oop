fea4cd047917884246e2be869a2c24f8ee5f56eb
Joven Aberia <66535223+master90909@users.noreply.github.com>
2024-11-16 22:50:02 +0800

    Create LA_30.py

---
+++ LA/LA_30.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+def print_anime():
+    anime_title = "One Punch Man"
+    print(f"My favorite anime is {anime_title}")
+
+window = tk.Tk()
+window.title("Anime print")
+window.geometry("300x400")
+
+button = tk.Button(window, text="Run", command=print_anime)
+button.pack(pady=20)
+
+window.mainloop()
