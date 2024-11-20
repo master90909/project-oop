5418ca4a48103cdf29ddfac158f23a02a8e02bd1
last <m99975999@gmail.com>
2024-11-20 22:30:18 +0800

    deleted:    LA/la_1.py  	LA/LA_1.py  	LA/LA_3.py  	LA/LA_4.py  	LA/LA_5.py  	LA/LA_6.py  	LA/LA_7.py  	LA/LA_8.py  	LA/LA_9.py

--- LA/la_1.py
+++
@@ -1,8 +0,0 @@
-class hero_name:
-    hero = "layla"
-    role = "markmans"
-    attack_type = "long range"
-
-p1 = hero_name()
-
-print(f"Name Hero: {p1.hero}, Role: {p1.role}, attack Type: {p1.attack_type}")
