6116bdf518979f5ae14419f4f5219f66f4f9ac7e
Joven Aberia <66535223+master90909@users.noreply.github.com>
2024-11-20 14:44:41 +0800

    Create LA_20.py

---
+++ LA/LA_20.py
@@ -0,0 +1,13 @@
+class Favorite_food():
+    def __init__(self,name,main_ingredient,onion,carrot,):
+        self.__name = name
+        self._main_ingredient = main_ingredient
+        self.__onion = onion
+        self._carrot = carrot
+
+    def __str__(self):
+        return f"my favorite food is {self.__name} and the mian ingredient {self._main_ingredient}, {self.__onion}, {self._carrot}"
+    def getter(self):
+        return self._main_ingredient, self._carrot
+food1 = Favorite_food("lumpia_shanghai", "baboy" , "sibuyas" , "karot")
+print(food1.getter())
