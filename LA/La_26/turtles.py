b21e7fa1a7884613e8acb6c41349563d24ef5620
Joven Aberia <66535223+master90909@users.noreply.github.com>
2024-11-20 14:39:53 +0800

    Create turtles.py

---
+++ LA/La_26/turtles.py
@@ -0,0 +1,46 @@
+import abc
+from abc import ABC, abstractmethod
+
+class NinjaTurtle(ABC):
+    @property
+    @abstractmethod
+    def alias(self):
+        pass
+
+class Leonardo(NinjaTurtle):
+    def __init__(self,name,alias):
+        self.name = name
+        self.__alias = alias
+    @property
+    def alias(self):
+        return self.__alias
+
+
+class Michelangelo(NinjaTurtle):
+
+    def __init__(self,name,alias):
+        self.name = name
+        self.__alias = alias
+    @property
+    def alias(self):
+        return self.__alias
+
+
+class Donatello(NinjaTurtle):
+
+    def __init__(self,name,alias):
+        self.name = name
+        self.__alias = alias
+    @property
+    def alias(self):
+        return self.__alias
+
+
+class Rapheal(NinjaTurtle):
+
+    def __init__(self,name,alias):
+        self.name = name
+        self.__alias = alias
+    @property
+    def alias(self):
+        return self.__alias
