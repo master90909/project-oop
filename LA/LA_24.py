16904da89e2e69cc30e77f6dacbfcd06c6c2a13b
last <m99975999@gmail.com>
2024-12-22 23:31:36 +0800

    new file:   LA_24.py

---
+++ LA/LA_24.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+class GameCharacter(ABC):
+    def __init__(self):
+        pass
+   
+    @abstractmethod
+    def attack(self):
+        pass
+
+class Warrior(GameCharacter):
+    def attack(self):
+        print("Swings sword!")
+
+class Mage(GameCharacter):
+    def attack(self):
+        print("Casts a fireball!")
+
+class Archer(GameCharacter):
+    def attack(self):
+        print("Shoots an arrow!")
+
+class Healer(GameCharacter):
+    def attack(self):
+        print("Casts healing spell on ally!")
+
+warrior = Warrior()
+mage = Mage()
+archer =  Archer()
+healer = Healer()
+
+for character in (warrior, mage, archer, healer):
+    character.attack()
